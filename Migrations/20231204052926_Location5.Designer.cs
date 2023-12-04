b99ed4b672928a4ab7e5eb6494c78bea265f9b60
arbaaz234 <arbaazghameriya@gmail.com>
2023-12-04 13:41:55 +0530

    HVDCPole Done

--- Migrations/20231204052926_Location5.Designer.cs
+++ Migrations/20231204052926_Location5.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EMS.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EMS.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20231204052926_Location5")]
+    partial class Location5
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.11");
