f96027c7b185309b782a45d04219188734d5ded9
Harsh Anil Hegishte <harsh.hegishte@spit.ac.in>
2023-12-04 15:27:49 +0530

    Harsh (#4)
    * (git) added migrations
    
    * Deleted EMS

---
+++ Migrations/20231129044309_HVDCPoleUpdated.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class HVDCPoleUpdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PoleNumber",
+                table: "Element");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PoleNumber",
+                table: "Element",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
