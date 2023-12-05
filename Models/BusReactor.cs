36696446468b7086607f12412e5d010292f71be3
Harsh Anil Hegishte <harsh.hegishte@spit.ac.in>
2023-12-05 16:46:49 +0530

    (pages) substation done. Bus and Bus Reactor fix needed! (#10)

--- Models/BusReactor.cs
+++ Models/BusReactor.cs
@@ -6,7 +6,7 @@ namespace EMS.Models
         public int ElementId { get; set; }
         public int BusId { get; set; }
         public int MVARCapacity { get; set; }
-        public string? BusName { get; set; }
+        public string? BusReactorName { get; set; }
 
         public Element? Element { get; set; }
         public Bus? Bus { get; set; }
--- Pages/Substations/Create.cshtml.cs
+++ Pages/Substations/Create.cshtml.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using EMS.Data;
 using EMS.Models;
 
 namespace EMS.Pages.Substations
@@ -21,15 +16,20 @@ namespace EMS.Pages.Substations
 
         public IActionResult OnGet()
         {
-        ViewData["LocationId"] = new SelectList(_context.Location, "LocationId", "LocationId");
-        ViewData["VoltageId"] = new SelectList(_context.Voltage, "VoltageId", "VoltageId");
+            ViewData["Owners"] = new SelectList(_context.Owner, "OwnerId", "OwnerName");
+            ViewData["LocationId"] = new SelectList(_context.Location, "LocationId", "LocationName");
+            ViewData["VoltageId"] = new SelectList(_context.Voltage, "VoltageId", "VoltageLevel");
+            ViewData["SubstationType"] = new SelectList(new List<string> { "AC", "DC" });
             return Page();
         }
 
         [BindProperty]
-        public Substation Substation { get; set; } = default!;
+        public Substation? Substation { get; set; } = default!;
+        [BindProperty]
+        public Owner? Owner { get; set; } = default!;
+        [BindProperty]
+        public OwnerSubstation? OwnerSubstation { get; set; } = default!;
         
-
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
@@ -40,6 +40,20 @@ namespace EMS.Pages.Substations
 
             _context.Substation.Add(Substation);
             await _context.SaveChangesAsync();
+
+            var lastSubstationId = _context.Substation.Max(e => e.SubstationId);
+
+            foreach (var ownerId in Request.Form["Owners"]) // Assuming "Owners" is the name of the multiple select control
+            {
+                var ownersubstation = new OwnerSubstation
+                {
+                    OwnerId = Convert.ToInt32(ownerId), // Assuming OwnerId is an integer
+                    SubstationId = lastSubstationId
+                };
+
+                _context.OwnerSubstation.Add(ownersubstation);
+                await _context.SaveChangesAsync();
+            }
 
             return RedirectToPage("./Index");
         }
