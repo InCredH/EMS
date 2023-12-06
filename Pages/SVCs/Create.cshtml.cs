f60ad650c829fc5c05c93aba71c163437f6ee768
Arbaaz234 <89303021+Arbaaz234@users.noreply.github.com>
2023-12-06 17:27:19 +0530

    SVC Done (#16)
    * Maintainence
    
    * (Pages) SVC

--- Pages/SVCs/Create.cshtml.cs
+++ Pages/SVCs/Create.cshtml.cs
@@ -22,13 +22,18 @@ namespace EMS.Pages.SVCs
         public IActionResult OnGet()
         {
         ViewData["BusId"] = new SelectList(_context.Bus, "BusId", "BusId");
-        ViewData["ElementId"] = new SelectList(_context.Set<Element>(), "ElementId", "ElementId");
+            ViewData["Substation1Id"] = new SelectList(_context.Set<Substation>(), "SubstationId", "SubstationName");
+            ViewData["Owners"] = new SelectList(_context.Set<Owner>(), "OwnerId", "OwnerName");
+            ViewData["Locations"] = new SelectList(_context.Set<Location>(), "LocationId", "LocationName");
+            ViewData["LineType"] = new SelectList(new List<string> { "AC Transmission Line", "HVDC Line" });
             return Page();
         }
 
         [BindProperty]
         public SVC SVC { get; set; } = default!;
-        
+        [BindProperty]
+        public Element? Element { get; set; }
+
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
@@ -37,7 +42,27 @@ namespace EMS.Pages.SVCs
             {
                 return Page();
             }
+            Element.ElementType = "SVC";
+            DateTime Comm_utcDateTime = Element.CommissioningDate.ToUniversalTime();
+            DateTime DeComm_utcDateTime = Element.DecommissioningDate.ToUniversalTime();
 
+            Element.CommissioningDate = Comm_utcDateTime;
+            Element.DecommissioningDate = DeComm_utcDateTime;
+            _context.Element.Add(Element);
+            await _context.SaveChangesAsync();
+            var lastElementId = _context.Element.Max(e => e.ElementId);
+            SVC.ElementId = lastElementId;
+            foreach (var ownerId in Request.Form["Owners"]) // Assuming "Owners" is the name of the multiple select control
+            {
+                var elementOwner = new ElementOwner
+                {
+                    ElementId = lastElementId,
+                    OwnerId = Convert.ToInt32(ownerId) // Assuming OwnerId is an integer
+                };
+
+                _context.ElementOwner.Add(elementOwner);
+                await _context.SaveChangesAsync();
+            }
             _context.SVC.Add(SVC);
             await _context.SaveChangesAsync();
 
