853612529743370174fb679c36e8f23eed99a13d
Arbaaz234 <89303021+Arbaaz234@users.noreply.github.com>
2023-12-07 01:19:30 +0530

    Constraints for Bay Line Reactor and Generating Unit (#23)
    * Maintainence
    
    * (Constraints) Bay GU LineReactor

--- Pages/Bays/Create.cshtml.cs
+++ Pages/Bays/Create.cshtml.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using EMS.Models;
@@ -16,14 +17,27 @@ namespace EMS.Pages.Bays
 
         public IActionResult OnGet()
         {
-            ViewData["ConnectingElement1Id"] = new SelectList(_context.Set<Element>(), "ElementId", "ElementId");
+            ViewData["ConnectingElement1Id"] = new SelectList(_context.Set<Element>(), "ElementId", "ElementType");
             ViewData["Substation1Id"] = new SelectList(_context.Set<Substation>(), "SubstationId", "SubstationName");
-            ViewData["ConnectingElement2Id"] = new SelectList(_context.Set<Element>(), "ElementId", "ElementId");
+            ViewData["ConnectingElement2Id"] = new SelectList(_context.Set<Element>(), "ElementId", "ElementType");
             ViewData["ElementId"] = new SelectList(_context.Set<Element>(), "ElementId", "ElementId");
             ViewData["VoltageId"] = new SelectList(_context.Voltage, "VoltageId", "VoltageLevel");
             ViewData["BayType"] = new SelectList(Enum.GetValues(typeof(BayType)));
             ViewData["LocationId"] = new SelectList(_context.Set<Location>(), "LocationId", "LocationName");
             ViewData["Owners"] = new SelectList(_context.Set<Owner>(), "OwnerId", "OwnerName");
+            ViewData["ConnectingElement3Id"] = new SelectList(
+                _context.Set<Element>()
+                    .Join(_context.Set<Substation>(),
+                          element => element.Substation1Id,
+                          substation => substation.SubstationId,
+                          (element, substation) => new
+                          {
+                              ElementId = element.ElementId,
+                              DisplayText = $"{element.ElementType} - {substation.SubstationName} - {element.ElementNumber}"
+                          }),
+                "ElementId",
+                "DisplayText"
+            );
             return Page();
         }
 
@@ -44,6 +58,54 @@ namespace EMS.Pages.Bays
             if(Element == null || Bay == null) {
                 Console.WriteLine("Element or Bay is null");
                 return Page();
+            }
+            var substation1IdConnectingElement1 = _context.Element
+               .Where(e => e.ElementId == Bay.ConnectingElement1Id)
+               .Select(e => e.Substation1Id)
+               .FirstOrDefault();
+
+            var substation1IdConnectingElement2 = _context.Element
+                .Where(e => e.ElementId == Bay.ConnectingElement2Id)
+                .Select(e => e.Substation1Id)
+                .FirstOrDefault();
+            
+            if (substation1IdConnectingElement1 != substation1IdConnectingElement2)
+            {
+                ModelState.AddModelError("Bay.ConnectingElement2Id", "Connecting Element 1 and 2 should have the same Substation.");
+                return Page();
+            }
+            var buscheck1id = _context.Element
+                   .Where(e => e.ElementId == Bay.ConnectingElement1Id)
+                   .Select(e => e.ElementType)
+                   .FirstOrDefault();
+            var buscheck2id = _context.Element
+                   .Where(e => e.ElementId == Bay.ConnectingElement2Id)
+                   .Select(e => e.ElementType)
+                   .FirstOrDefault();
+            if (Bay.BayType == "Tie")
+            {
+                
+                if(buscheck1id=="Bus" || buscheck2id == "Bus")
+                {
+                    ModelState.AddModelError("Bay.ConnectingElement2Id", "In Type Tie, A connecting element cannot be a Bus.");
+                    return Page();
+                }
+            }
+            if (Bay.BayType == "Main")
+            {
+                if ((buscheck1id != "Bus" && buscheck2id != "Bus") || buscheck1id==buscheck2id)
+                {
+                    ModelState.AddModelError("Bay.ConnectingElement2Id", "In Type Main, exactly 1 element can be a Bus.");
+                    return Page();
+                }
+            }
+            if(Bay.BayType=="Bus_Coupler" || Bay.BayType == "Bus_Sectionalizer" || Bay.BayType == "TBC")
+            {
+                if (buscheck1id != "Bus" || buscheck2id != "Bus" || buscheck1id!=buscheck2id )
+                {
+                    ModelState.AddModelError("Bay.ConnectingElement2Id", "In Type Bus_Coupler,Bus_Sectionalizer or TBC, both connecting element should be Buses.");
+                    return Page();
+                }
             }
             // Set the element data
             Element.ElementType = "Bay";
